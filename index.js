25ffd58b06fc64cd9469da934e1b7d9271de81ad
Sihle Mbobo <khwezimbobo@gmail.com>
2023-11-23 00:37:31 +0200

    made some changes

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-let input1 = document.getElementById('red').value
+let output = document.getElementById('hex');
 
-let input2 = document.getElementById('green').value
+let btn = document.getElementById('calc');
 
-let input3 = document.getElementById('blue').value
+function rgbToHex() {
+    let input1 = parseInt(document.getElementById('red').value, 10);
+    let input2 = parseInt(document.getElementById('green').value, 10);
+    let input3 = parseInt(document.getElementById('blue').value, 10);
 
-let output = document.getElementById('hex')
+    function toHex(color) {
+        let hex = color.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+    }
 
-let btn = document.getElementById('calc')
+    let RED = toHex(input1);
+    let GRN = toHex(input2);
+    let BLU = toHex(input3);
 
-function rgbToHex(input1,input2,input3) {
-   let toHex = (color) => {
-    let hex = color.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-   };
-let RED = toHex(input1)
-
-let GRN = toHex(input2)
-
-let BLU = toHex(input3)
-
-output.value = "#" + RED + GRN + BLU
+    output.value = "#" + RED + GRN + BLU;
 }
 
-btn.addEventListener('click',rgbToHex)
+btn.addEventListener('click', rgbToHex);
